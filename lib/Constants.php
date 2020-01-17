89fd2a23be24e571c448026e8c1a92e4ac4645c6
wilddebug <andriy.miga@gmail.com>
2020-01-17 12:20:59 +0200

    add constants

---
+++ lib/Constants.php
@@ -0,0 +1,17 @@
+<?php
+
+class Constants
+{
+    const INDUSTRY_TYPES = [
+        1 => 'Biotech Organization',
+        2 => 'University',
+        3 => 'Government Research Organization',
+        4 => 'Hospital/Clinical Services',
+        5 => 'Research Institute',
+        6 => 'Contract Research Organization',
+        7 => 'Diagnostic/CLIA Testing Lab',
+        8 => 'Medical Device Manufacturing',
+        9 => 'Research Device Manufacturing',
+        10 => 'Comprehensive Cancer Center',
+    ];
+}
